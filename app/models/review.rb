c2119bb3a584fe64506f60da0f6b506aa54eabbd
Max Martinez <maximartinez@me.com>
2023-05-29 17:21:46 +0100

    added validations to the review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :booking
   validates :content, :rating, presence: true
+  validates :rating, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
+  validates :content, length: { minumum: 10, message: "must be at least 10 characters long" }
 end
