8d8865a321af03932871ed2ca3eb5db95a8b8a23
hasna begum <h.b.muni16@gmail.com>
2023-05-29 17:10:28 +0100

    validated planes model

--- app/models/plane.rb
+++ app/models/plane.rb
@@ -3,4 +3,7 @@ class Plane < ApplicationRecord
   has_many :bookings
 
   validates :pax_capacity, :fh_price, :fh_range, :model, :location, presence: true
+  validates :pax_capacity, numericality: { only_integer: true }
+  validates :fh_price, numericality: { only_float: true }
+  validates :fh_range, numericality: { only_float: true }
 end
