2eb1dd73235af09017658e5b45318a19d7e73ec5
Jamie Graham <jagraham04@yahoo.com>
2023-05-29 18:00:55 +0100

    Created bookings validations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,19 @@
 class Booking < ApplicationRecord
-  belongs_to :user
-  belongs_to :plane
+  # belongs_to :user
+  # belongs_to :plane
   has_many :reviews
+
   validates :total_hours, :total_price, :date, presence: true
+  validates :total_hours, numericality: { only_float: true }
+  validates :total_price, numericality: { only_float: true }
+  validate :total_hours_greater_than_one
+  validate :date_cannot_be_in_the_past
+
+  def date_cannot_be_in_the_past
+    errors.add(:date, "Booking date can't be in the past!") if date.past?
+  end
+
+  def total_hours_greater_than_one
+    errors.add(:total_hours, "The minimum flight is one hour!") if total_hours < 1.0
+  end
 end
