a134e251f443208f52d79c094db468d131dd788f
hasna begum <h.b.muni16@gmail.com>
2023-05-30 16:51:04 +0100

    completed booking form

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,28 @@
 class BookingsController < ApplicationController
+
+  def index
+    @bookings = Booking.all
+  end
+
   def new
-    @booking = Booking.all
+    @booking = Booking.new
+    @plane = Plane.find(params[:plane_id])
+  end
+
+  def create
+    @plane = Plane.find(params[:plane_id])
+    @booking = Booking.new(booking_params)
+    @booking.total_price = booking_params["total_hours"] * @plane.fh_price
+    @booking.user_id = current_user.id
+    @booking.plane_id = @plane.id
+    if @booking.save
+      redirect_to plane_bookings_path(@plane)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def booking_params
+    params.require(:booking).permit(:date, :total_hours, :total_price)
   end
 end
