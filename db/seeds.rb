457cf5515b59368cc43d5678c51987bcbe9a2718
Eduardo Guerrero <eduardo.guerrero.lara@gmail.com>
2023-05-30 14:14:45 +0100

    Two planes added to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
 puts 'Cleaning data base'
 Plane.destroy_all
 puts "Creating plane model"
-planes = Plane.create([
-  {model: 'Cessna 510', location: 'LHR', pax_capacity: 4, fh_range: 3, fh_price: 1700}
-])
+planes = Plane.create!(
+  [{ model: 'Cessna 510', location: 'LHR', pax_capacity: 4, fh_range: 3, fh_price: 1700, user_id: 1 },
+    { model: 'Learjet', location: 'LHR', pax_capacity: 8, fh_range: 5, fh_price: 2800, user_id:2 }]
+)
 puts "#{planes.length} planse created"
