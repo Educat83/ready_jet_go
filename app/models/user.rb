3226983122105e70f687f41ef435018ab1733657
Jamie Graham <jagraham04@yahoo.com>
2023-05-31 15:16:10 +0100

    Added user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   has_many :bookings
   has_many :planes, dependent: :destroy
 
+  validates :first_name, :last_name, presence: true
+  validates :email, uniqueness: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
