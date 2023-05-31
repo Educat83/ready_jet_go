89258d4c0464ee0fac3b4a7e10672c1d212e50d3
Eduardo Guerrero <eduardo.guerrero.lara@gmail.com>
2023-05-31 14:01:36 +0100

    Booking edit and update added

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,15 +1,17 @@
 class BookingsController < ApplicationController
-  before_action :set_booking, only: %i[new crate edit update]
+  # before_action :set_booking, only: %i[index new create edit update]
 
   def index
     @bookings = Booking.all
   end
 
   def new
+    @plane = Plane.find(params[:plane_id])
     @booking = Booking.new
   end
 
   def create
+    @plane = Plane.find(params[:plane_id])
     @booking = Booking.new(booking_params)
     @booking.total_price = booking_params["total_hours"].to_i * @plane.fh_price
     @booking.user = current_user
@@ -21,11 +23,16 @@ class BookingsController < ApplicationController
     end
   end
 
-  def edit; end
+  def edit
+    @plane = Plane.find(params[:plane_id])
+    @booking = Booking.find(params[:id])
+  end
 
   def update
-    if @pbooking.update(booking_params)
-      redirect_to booking_path(@booking), notice: "#{@booking.id} was successfully updated."
+    @plane = Plane.find(params[:plane_id])
+    @booking = Booking.find(params[:id])
+    if @booking.update(booking_params)
+      redirect_to bookings_path(@booking), notice: "#{@booking.id} was successfully updated."
     else
       render :edit
     end
@@ -33,9 +40,9 @@ class BookingsController < ApplicationController
 
   private
 
-  def set_booking
-    @plane = Plane.find(params[:plane_id])
-  end
+  # def set_booking
+  #   @plane = Plane.find(params[:plane_id])
+  # end
 
   def booking_params
     params.require(:booking).permit(:date, :total_hours, :total_price)
